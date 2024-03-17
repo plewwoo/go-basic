7860ae5cad4cd387acfa8a04709908c6d8112195
Plew Woothiphom <pew_woothiphom@hotmail.com>
2024-03-17 11:01:14 +0700

    sort

--- service/book_service.go
+++ service/book_service.go
@@ -7,9 +7,9 @@ import (
 )
 
 type BookService interface {
+	FindAll(ctx context.Context) []response.BookReponse
+	FindById(ctx context.Context, bookId int) (response.BookReponse, interface{})
 	Create(ctx context.Context, request request.BookCreateRequest) response.BookReponse
 	Update(ctx context.Context, request request.BookUpdateRequest) response.BookReponse
 	Delete(ctx context.Context, bookId int) response.BookReponse
-	FindById(ctx context.Context, bookId int) (response.BookReponse, interface{})
-	FindAll(ctx context.Context) []response.BookReponse
 }
